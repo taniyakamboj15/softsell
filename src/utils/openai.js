755010d54d34fb160a52304e2d0a23075c72167b
Taniya Kamboj <taniyakamboj184@gmail.com>
2025-05-10 17:31:17 +0530

    Final updat

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,14 +1,5 @@
 import OpenAI from "openai";
 
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// const openai = new OpenAI({
-//   apiKey: process.env.OPENAI_API_KEY,
-//   baseURL: process.env.API_URL,
-
-//   dangerouslyAllowBrowser: true,
-// });
 const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   baseURL: import.meta.env.VITE_API_URL,
